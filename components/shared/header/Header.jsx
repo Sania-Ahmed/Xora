cbfe74aed66cf8436446f23d77ceef0174297343
Sania-Ahmed <tasminsania77@gmail.com>
2024-12-12 11:50:49 +0600

    Initial commit

---
+++ components/shared/header/Header.jsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useState } from "react"
+
+const Item = ( {title}) => (
+  <p className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">{title}</p>
+
+)
+const Header = () => {
+  const [isOpen , setIsOpen] = useState(false);
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <div className=" container flex h-14 items-center max-lg:px-5">
+        <a href="/" className="lg:hidden flex-1 cursor-pointer z-5  ">      
+        <img src="/images/xora.svg" alt="logo" width={115} height={56}/>
+        </a>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          
+        <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:overflow-hidden sidebar-before max-md:px-4 ">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+                <ul className="flex max-lg:block max-lg:px-12">
+                    <li className="nav-li">
+                      <Item title={'Features'}/>
+                      <div className="dot"/>
+                      <Item title={'pricing'}/>
+                    </li>
+                    <li className="nav-logo">
+                       <img src="/images/xora.svg" width={160} height={55} alt="logo"></img>
+                    </li>
+                    <li className="nav-li">
+                    <Item title={'Faq'}/>
+                    <div className="dot"></div>
+                    <Item title={'Contact'}/>
+                    </li>
+
+                </ul> 
+            </nav>
+
+        </div>
+        </div>
+        <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center" onClick={() => setIsOpen(!isOpen)}>
+        <img className="size-1/2 object-contain" src={`images/${isOpen ? "close" : "magic"}.svg`} alt="icon" />
+        </button>
+    </div>
+    </header>
+  )
+}
+
+export default Header
